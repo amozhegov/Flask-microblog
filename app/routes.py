1dc530ee1f33b5d5b78817e96137ee606d1e84da
amozhegov <kaneImfb@gmail.com>
2023-09-24 14:57:09 +0200

    Added user profile

--- app/routes.py
+++ app/routes.py
@@ -66,3 +66,15 @@ def index():
     return render_template('index.html', 
                            title = 'Main page', 
                            posts = posts)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post1'},
+        {'author': user, 'body': 'Test post2'}
+    ]
+    return render_template('user.html', 
+                           user=user, 
+                           posts=posts)
