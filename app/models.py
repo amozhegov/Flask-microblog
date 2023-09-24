0dd0e85bb45508e72d8d450feaac3bfcd47d9c94
amozhegov <kaneImfb@gmail.com>
2023-09-24 16:00:33 +0200

    Added gravatars

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login
+from hashlib import md5
 
 @login.user_loader
 def load_user(id):
@@ -21,6 +22,10 @@ class User(db.Model, UserMixin):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
