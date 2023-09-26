e20bc951a37dca4a53342d570f204abad3cd6d1a
amozhegov <kaneImfb@gmail.com>
2023-09-26 20:53:38 +0200

    Added error handlers for 500 and 404

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,13 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 
 app = Flask(__name__)
+# Enable debug mode
+#app.debug = True
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
-from app import routes, models
+from app import routes, models, errors
+
